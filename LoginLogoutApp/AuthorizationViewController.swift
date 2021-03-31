5da00431627e6108f7eb377cb3e4f3551d0cb24f
Ivan Demchenko <vanuademchenko@gmail.com>
2021-04-01 00:25:03 +0300

    Initial Commit

---
+++ LoginLogoutApp/AuthorizationViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  LoginLogoutApp
+//
+//  Created by Ivan Demchenko on 31.03.21.
+//
+
+import UIKit
+
+class AuthorizationViewController: UIViewController, UITextFieldDelegate {
+    // MARK: - IB Outlets
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    // MARK: - Private Properties
+    private let userName = "user"
+    private let password = "password"
+    
+    // MARK: - Override Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userNameTF.delegate = self
+        passwordTF.delegate = self
+    }
+    // Метод для скрытия клавиатуры тапом по экрану
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? InAppViewController else { return }
+        destination.userName = userNameTF.text
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        guard unwindSegue.source is InAppViewController else { return }
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+    @IBAction func logInBtnPressed() {
+        checkUsernameAndPassword()
+    }
+    @IBAction func forgotUserNamePresser() {
+        showAlert(title: "Don't worry!", message: "Your username is \(userName)")
+    }
+    @IBAction func forgotPasswordPresser() {
+        showAlert(title: "Don't worry!", message: "Your password is \(password)")
+    }
+    
+    // MARK: - Public Methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {//delegate method
+        if textField == userNameTF {
+            passwordTF.becomeFirstResponder()
+        } else if textField == passwordTF {
+            passwordTF.resignFirstResponder()
+            checkUsernameAndPassword()
+            performSegue(withIdentifier: "logInIdentifier", sender: .none)
+        }
+        return true
+    }
+    // MARK: - Private Methods
+    private func checkUsernameAndPassword() {
+        if userNameTF.text != userName {
+            showAlert(title: "Error", message: "The username you entered doesn't appear to belong to an account.\n Please check your username and try again.")
+        } else if passwordTF.text != password {
+            showAlert(title: "Error", message: "The password you entered doesn't appear to belong to an account.\n Please check your password and try again.")
+            passwordTF.text = ""
+        }
+    }
+    
+}
+
+
+// MARK: - Table View Data Source
+extension AuthorizationViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
---
+++ LoginLogoutApp/InAppViewController.swift
@@ -0,0 +1,25 @@
+//
+//  InAppViewController.swift
+//  LoginLogoutApp
+//
+//  Created by Ivan Demchenko on 31.03.21.
+//
+
+import UIKit
+
+class InAppViewController: UIViewController {
+    // MARK: - IB Outlets
+    @IBOutlet weak var userNameLabel: UILabel!
+    // MARK: - Public Properties
+    var userName: String!
+    // MARK: - Override Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userNameLabel.text = userName
+    }
+    // MARK: - IB Action
+    @IBAction func logoutBtnPressed() {
+        dismiss(animated: true)
+    }
+    
+}
